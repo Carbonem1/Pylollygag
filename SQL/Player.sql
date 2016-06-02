0ec1f11d4b5ff862d7deccf9b7f0b2e4b03da4a2
Carbonem1 <carbonem1@wit.edu>
2016-06-01 23:34:12 -0400

    Added SQL files

--- SQL/Player.sql
+++ SQL/Player.sql
@@ -3,19 +3,17 @@ CREATE schema draft1
 go
 
 -- create table
-CREATE table draft1.Player
+CREATE table testdb.Player
 (
 	PlayerID int PRIMARY KEY DEFAULT 0,
 	PlayerName char(30) NULL, 
-	ProfileIconId int NULL, 
-	SummonerLevel int NULL
 )
 
 exec sp_columns Player
 
 -- inset test data
-INSERT INTO draft1.Player(PlayerID, PlayerName, ProfileIconId, SummonerLevel)
-	VALUES (1, 'Recklessmike', 1, 30)
+INSERT INTO draft1.Player(PlayerID, PlayerName)
+	VALUES (1, 'Recklessmike')
 
 -- test query
 SELECT *
@@ -24,6 +22,3 @@ FROM draft1.Player
 -- find the host name and user
 SELECT HOST_NAME() AS HostName,
 	SUSER_NAME() LoggedInUser
-
--- set default schema
-ALTER USER WIT\carbonem1 WITH DEFAULT_SCHEMA = draft1
